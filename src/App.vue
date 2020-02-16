79aea649423c2c46eba882ee26e99fda6008e3b7
Sarah Dayan <dayan.sarah@gmail.com>
2020-02-16 02:37:15 +0100

    lint: use Algolia linter

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,34 @@
   <div id="app">
     <div class="container">
       <textarea class="editor" v-model="content" spellcheck="false" />
-      <div v-show="current.matches('visible.rendered')" class="render markdown-body" v-html="rendered" />
-      <pre v-show="current.matches('visible.raw')" class="render raw">{{ raw }}</pre>
+      <div
+        v-show="current.matches('visible.rendered')"
+        class="render markdown-body"
+        v-html="rendered"
+      />
+      <pre v-show="current.matches('visible.raw')" class="render raw">{{
+        raw
+      }}</pre>
     </div>
     <div class="toggle">
-      <button :aria-label="current.matches('visible.raw') ? 'Show rendered Markdown' : 'Show HTML code'" class="btn" v-show="current.matches('visible')" @click="send('SWAP')">
+      <button
+        :aria-label="
+          current.matches('visible.raw')
+            ? 'Show rendered Markdown'
+            : 'Show HTML code'
+        "
+        class="btn"
+        v-show="current.matches('visible')"
+        @click="send('SWAP')"
+      >
         <menu-icon v-show="current.matches('visible.raw')" />
         <code-icon v-show="current.matches('visible.rendered')" />
       </button>
-      <button :aria-label="current.matches('hidden') ? 'Show editor' : 'Hide editor'" class="btn" @click="send('TOGGLE')">
+      <button
+        :aria-label="current.matches('hidden') ? 'Show editor' : 'Hide editor'"
+        class="btn"
+        @click="send('TOGGLE')"
+      >
         <eye-icon v-show="current.matches('hidden')" />
         <eye-off-icon v-show="current.matches('visible')" />
       </button>
@@ -19,16 +38,16 @@
 </template>
 
 <script>
-import { Machine, interpret } from 'xstate'
-import MarkdownIt from 'markdown-it'
-import { indent } from 'indent.js'
+import { Machine, interpret } from 'xstate';
+import MarkdownIt from 'markdown-it';
+import { indent } from 'indent.js';
 
-import EyeIcon from '@/assets/icons/eye.svg'
-import EyeOffIcon from '@/assets/icons/eye-off.svg'
-import CodeIcon from '@/assets/icons/code.svg'
-import MenuIcon from '@/assets/icons/menu.svg'
+import EyeIcon from '@/assets/icons/eye.svg';
+import EyeOffIcon from '@/assets/icons/eye-off.svg';
+import CodeIcon from '@/assets/icons/code.svg';
+import MenuIcon from '@/assets/icons/menu.svg';
 
-const md = new MarkdownIt()
+const md = new MarkdownIt();
 
 const swapMachine = Machine({
   id: 'swap',
@@ -36,28 +55,28 @@ const swapMachine = Machine({
   states: {
     visible: {
       on: {
-        TOGGLE: 'hidden'
+        TOGGLE: 'hidden',
       },
       initial: 'rendered',
       states: {
         rendered: {
           on: {
-            SWAP: 'raw'
-          }
+            SWAP: 'raw',
+          },
         },
         raw: {
           on: {
-            SWAP: 'rendered'
-          }
-        }
-      }
+            SWAP: 'rendered',
+          },
+        },
+      },
     },
     hidden: {
       on: {
-        TOGGLE: 'visible'
-      }
-    }
-  }
+        TOGGLE: 'visible',
+      },
+    },
+  },
 });
 
 export default {
@@ -67,32 +86,32 @@ export default {
     return {
       swapService: interpret(swapMachine),
       current: swapMachine.initialState,
-      content: '# Hello there!\n\n- Type some Markdown on the left\n- See HTML in the right\n- Magic\n\n![An orange jellyfish](https://i.picsum.photos/id/1069/400/250.jpg)'
-    }
+      content: '# Hello there!\n\n- Type some Markdown on the left\n- See HTML in the right\n- Magic\n\n![An orange jellyfish](https://i.picsum.photos/id/1069/400/250.jpg)',
+    };
   },
   computed: {
     rendered() {
-      return md.render(this.content)
+      return md.render(this.content);
     },
     raw() {
       return indent.html(this.rendered, {
-        tabString: '  '
-      })
-    }
+        tabString: '  ',
+      });
+    },
   },
   methods: {
     send(event) {
-      this.swapService.send(event)
-    }
+      this.swapService.send(event);
+    },
   },
   created() {
     this.swapService
       .onTransition(state => {
-        this.current = state
+        this.current = state;
       })
-      .start()
+      .start();
   },
-}
+};
 </script>
 
 <style src="./assets/styles.css" />
